a25417e7f6181383c78bf2a8fcf0a7db1298df68
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-06-30 00:57:36 +0200

    added serializing and deserialising to class

--- RecipesExercise1/RecipesExercise1/Category.cs
+++ RecipesExercise1/RecipesExercise1/Category.cs
@@ -1,18 +1,22 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace RecipesExercise1
 {
-    [Serializable]
-    internal class Category
+    public class Category
     {
-        string Id { get; set; }
-        string Name { get; set; }
+        [JsonIgnore]
+        private static readonly string _fileName = "categories.json";
+        public Guid Id { get; set; }
+        public string Name { get; set; }
 
         public Category()
         {
-            Id = Guid.NewGuid().ToString(); // Generate a unique ID for each category
+            Id = Guid.NewGuid(); // Generate a unique ID for each category
             Name = string.Empty;
         }
 
-        public Category(string id, string name)
+        public Category(Guid id, string name)
         {
             Id = id;
             Name = name;
@@ -20,8 +24,35 @@ namespace RecipesExercise1
 
         public Category(string name)
         {
-            Id = Guid.NewGuid().ToString(); // Generate a unique ID for each category
+            Id = Guid.NewGuid(); // Generate a unique ID for each category
             Name = name;
+        }
+        public static async Task<List<Category>?> GetCategory()
+        {
+            try
+            {
+                string text = await FileHandler.ReadAsync(_fileName);
+                return await JsonHandler.DeserializeAsync<List<Category>?>(text);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return new List<Category>();
+            }
+        }
+
+        public static async Task<bool> SaveCategory(List<Category> categories)
+        {
+            try
+            {
+                var json = await JsonHandler.SerializeAsync<List<Category>>(categories);
+                return await FileHandler.WriteAsync(_fileName, json);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
         }
 
         public static void EditCategory(Category category, string newName)
