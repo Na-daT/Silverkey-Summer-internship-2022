b4342abdd3f3e51eb37dc4749943d9ec743ac7df
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-06-29 13:23:09 +0200

    Create FileHandler.cs

---
+++ RecipesExercise1/RecipesExercise1/FileHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesExercise1
+{
+    internal class FileHandler
+    {
+        private static string _path = Path.Combine(Directory.GetCurrentDirectory(), "database.json");
+
+        public static async Task<FileStream> ReadFile()
+        {
+            FileStream openStream = File.OpenRead(_path);
+            return openStream;
+        }
+
+        public static async Task<bool> WriteFile(string? json)
+        {
+            try
+            {
+                await File.WriteAllTextAsync(_path, json);
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    }
+}
