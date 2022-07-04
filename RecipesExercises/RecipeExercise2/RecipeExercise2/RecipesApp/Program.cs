9717b56796618e553f69826f063cfdc668a12142
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-07-05 00:31:54 +0200

    initial commit

--- RecipesExercises/RecipeExercise2/RecipeExercise2/RecipesApp/Program.cs
+++ RecipesExercises/RecipeExercise2/RecipeExercise2/RecipesApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Web.Http;
+using System.Text;
+using System.Net.Http.Json;
+
+namespace RecipesApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var app = new ConsoleApp();
+            await app.RunMain();
+        }
+    }
+}
