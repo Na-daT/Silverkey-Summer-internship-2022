2520ca8a419411540d708ca5f03c57341b174e68
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-07-06 13:56:32 +0200

    logging

--- RecipesExercises/RecipeExercise2/RecipeExercise2/JsonAPI/Program.cs
+++ RecipesExercises/RecipeExercise2/RecipeExercise2/JsonAPI/Program.cs
@@ -1,13 +1,17 @@
-using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System;
 using System.Collections.Generic;
+using System.Web.Http;
 //using System.Web.Http.Results;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Logging.SetMinimumLevel(LogLevel.Warning);
+builder.Logging.AddConsole();
+
 
 var app = builder.Build();
 var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
@@ -38,9 +42,10 @@ app.MapPost("api/json/add-recipe", async ([FromBody] Recipe recipeToPost) =>
         await FileHandler.WriteAsync("recipe.json", json);
         return Results.Ok();
     }
-    catch
+    catch (Exception e)
     {
-        return Results.NotFound();
+        app.Logger.LogError(e.Message);
+        return Results.StatusCode(500);
     }
 });
 
@@ -48,35 +53,41 @@ app.MapPut("api/json/update-recipe", async ([FromBody] Recipe recipeToUpdate) =>
 {
     try
     {
-        var recipes = await File.ReadAllTextAsync("recipe.json");
-        var recipesList = JsonSerializer.Deserialize<List<Recipe>>(recipes, options);
+        var recipes = await FileHandler.ReadAsync("recipe.json");
+        var recipesList = await JsonHandler.DeserializeAsync<List<Recipe>>(recipes);
         var recipe = recipesList.FirstOrDefault(x => x.Id == recipeToUpdate.Id);
+        if (recipe == null)
+        {
+            return Results.StatusCode(404);
+        }
         recipesList[recipesList.IndexOf(recipe)] = recipeToUpdate;
-        var json = JsonSerializer.Serialize(recipesList, options);
-        await File.WriteAllTextAsync("recipe.json", json);
+        var json = await JsonHandler.SerializeAsync(recipesList);
+        await FileHandler.WriteAsync("recipe.json", json);
         return Results.Ok();
     }
-    catch
+    catch (Exception e)
     {
-        return Results.NotFound();
+        app.Logger.LogError(e.Message);
+        return Results.StatusCode(500);
     }
 });
 
-app.MapDelete("api/json/delete-recipe/{id}", async ([FromBody] Guid id) =>
+app.MapDelete("api/json/delete-recipe/{id}", async (Guid id) =>
 {
     try
     {
-        var recipes = await File.ReadAllTextAsync("recipe.json");
-        var recipesList = JsonSerializer.Deserialize<List<Recipe>>(recipes, options);
+        var recipes = await FileHandler.ReadAsync("recipe.json");
+        var recipesList = await JsonHandler.DeserializeAsync<List<Recipe>>(recipes);
         var recipe = recipesList.FirstOrDefault(x => x.Id == id);
         recipesList.Remove(recipe);
-        var json = JsonSerializer.Serialize(recipesList, options);
-        await File.WriteAllTextAsync("recipe.json", json);
+        var json = await JsonHandler.SerializeAsync(recipesList);
+        await FileHandler.WriteAsync("recipe.json", json);
         return Results.Ok();
     }
-    catch
+    catch (Exception e)
     {
-        return Results.NotFound();
+        app.Logger.LogError(e.Message);
+        return Results.StatusCode(500);
     }
 });
 
@@ -84,16 +95,17 @@ app.MapPost("api/json/add-category", async ([FromBody] Category categoryToPost) 
 {
     try
     {
-        var categories = await File.ReadAllTextAsync("category.json");
-        var categoriesList = JsonSerializer.Deserialize<List<Category>>(categories, options);
+        var categories = await FileHandler.ReadAsync("category.json");
+        var categoriesList = await JsonHandler.DeserializeAsync<List<Category>>(categories);
         categoriesList.Add(categoryToPost);
-        var json = JsonSerializer.Serialize(categoriesList, options);
-        await File.WriteAllTextAsync("category.json", json);
+        var json = await JsonHandler.SerializeAsync(categoriesList);
+        await FileHandler.WriteAsync("category.json", json);
         return Results.Ok();
     }
-    catch
+    catch (Exception e)
     {
-        return Results.NotFound();
+        app.Logger.LogError(e.Message);
+        return Results.StatusCode(500);
     }
 });
 
@@ -101,17 +113,18 @@ app.MapPut("api/json/update-category", async ([FromBody] Category categoryToUpda
 {
     try
     {
-        var categories = await File.ReadAllTextAsync("category.json");
-        var categoriesList = JsonSerializer.Deserialize<List<Category>>(categories, options);
+        var categories = await FileHandler.ReadAsync("category.json");
+        var categoriesList = await JsonHandler.DeserializeAsync<List<Category>>(categories);
         var category = categoriesList.FirstOrDefault(x => x.Id == categoryToUpdate.Id);
         category.Name = categoryToUpdate.Name;
-        var json = JsonSerializer.Serialize(categoriesList, options);
-        await File.WriteAllTextAsync("category.json", json);
+        var json = await JsonHandler.SerializeAsync(categoriesList);
+        await FileHandler.WriteAsync("category.json", json);
         return Results.Ok();
     }
-    catch
+    catch (Exception e)
     {
-        return Results.NotFound();
+        app.Logger.LogError(e.Message);
+        return Results.StatusCode(500);
     }
 });
 
