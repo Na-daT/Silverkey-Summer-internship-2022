0b8b3127c3aaf60189d28f1f0d824ed989a452ab
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-07-13 01:45:14 +0200

    httpClient configuration

--- RecipesExercises/RecipeExercise3/Program.cs
+++ RecipesExercises/RecipeExercise3/Program.cs
@@ -2,7 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+var AppName = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("api")["url"];
+builder.Services.AddHttpClient("recipeClient", httpClient =>
+{
+    httpClient.BaseAddress = new Uri(AppName);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
