e290b89295aa64f62efc04ab9707d3234be8c26b
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-07-18 02:36:17 +0200

    matching categories in recipe

--- RecipesExercises/RecipeExercise3/Models/Recipe.cs
+++ RecipesExercises/RecipeExercise3/Models/Recipe.cs
@@ -7,10 +7,15 @@ public class Recipe
     public Guid Id { get; set; }
     [Required]
     public string Title { get; set; }
-    public List<string> Ingredients { get; set; }
-    public List<string> Instructions { get; set; }
+    public List<string> Ingredients { get; set; } = new();
+    public List<string> Instructions { get; set; } = new();
     [Required]
-    public List<Category> Categories { get; set; }
+    public List<Category> Categories { get; set; } = new();
+
+    public Recipe()
+    {
+        Id = Guid.NewGuid();
+    }
 
     public static List<Recipe> Load(List<Category> categories, List<Recipe> recipes)
     {
@@ -22,5 +27,19 @@ public class Recipe
                 recipes[i].Categories[j] = categories.First(y => y.Id == recipes[i].Categories[j].Id);
 
         return recipes;
+    }
+    public static Recipe MatchCategory(Recipe recipe, List<Category> categories, List<Guid> categoriesIds)
+    {
+        ArgumentNullException.ThrowIfNull(recipe);
+        ArgumentNullException.ThrowIfNull(categories);
+        ArgumentNullException.ThrowIfNull(categoriesIds);
+        for (int i = 0; i < categoriesIds.Count; i++)
+        {
+            var category = categories.FirstOrDefault(y => y.Id == categoriesIds[i]);
+            if (category is null)
+                throw new Exception("Could not find category");
+            recipe.Categories.Add(category);
+        }
+        return recipe;
     }
 }
