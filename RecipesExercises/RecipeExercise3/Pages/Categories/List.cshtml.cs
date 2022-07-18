f8384618b8a5721f11228614e5a6e403cbd7c530
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-07-18 02:35:41 +0200

    List categories done

--- RecipesExercises/RecipeExercise3/Pages/Categories/List.cshtml.cs
+++ RecipesExercises/RecipeExercise3/Pages/Categories/List.cshtml.cs
@@ -1,19 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeExercise3.Models;
 
 namespace RecipeExercise3.Pages;
-
 public class CategoriesModel : PageModel
 {
     private readonly ILogger<CategoriesModel> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+    public List<Category> Categories { get; set; } = new();
 
-    public CategoriesModel(ILogger<CategoriesModel> logger)
+    public CategoriesModel(ILogger<CategoriesModel> logger, IHttpClientFactory httpClientFactory)
     {
         _logger = logger;
+        _httpClientFactory = httpClientFactory;
     }
 
-    public void OnGet()
+    public async Task OnGetAsync()
     {
-
+        var httpClient = _httpClientFactory.CreateClient("recipeClient");
+        var categoriesList = await httpClient.GetFromJsonAsync<List<Category>>("category");
+        if (categoriesList is null)
+            throw new Exception("Could not get categories");
+        Categories = categoriesList;
     }
 }
