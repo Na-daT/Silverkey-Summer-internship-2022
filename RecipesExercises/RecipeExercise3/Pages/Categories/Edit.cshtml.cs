248c5e16d1e7c14401b4f2e2927a0e6391e6553c
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-07-18 02:35:51 +0200

    edit categories done

---
+++ RecipesExercises/RecipeExercise3/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeExercise3.Models;
+
+namespace RecipeExercise3.Pages.Categories;
+
+public class CategoriesEditModel : PageModel
+{
+    private readonly ILogger<CategoriesEditModel> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    [BindProperty]
+    public Category UpdatedCategory { get; set; } = new();
+
+    public CategoriesEditModel(ILogger<CategoriesEditModel> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task OnGetAsync(string id)
+    {
+        var httpClient = _httpClientFactory.CreateClient("recipeClient");
+        var categoriesList = await httpClient.GetFromJsonAsync<List<Category>>("category");
+        if (categoriesList is null)
+            throw new Exception("Could not get categories");
+        UpdatedCategory = categoriesList.Find(c => c.Id.ToString() == id);
+    }
+
+    public async Task<IActionResult> OnPostAsync(string id)
+    {
+        var httpClient = _httpClientFactory.CreateClient("recipeClient");
+        var categoryToUpdate = new Category { Id = Guid.Parse(id), Name = UpdatedCategory.Name };
+        var result = await httpClient.PutAsJsonAsync("categories", categoryToUpdate);
+        if (result.IsSuccessStatusCode)
+            return RedirectToPage("/Categories/List");
+        throw new Exception("Could not update category");
+    }
+}
