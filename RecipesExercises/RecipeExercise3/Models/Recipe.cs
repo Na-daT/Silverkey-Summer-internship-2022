d876b6a65d5b4a105aa7de00096ecd66ac4dea26
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-07-18 03:14:58 +0200

    format

--- RecipesExercises/RecipeExercise3/Models/Recipe.cs
+++ RecipesExercises/RecipeExercise3/Models/Recipe.cs
@@ -21,13 +21,13 @@ public class Recipe
     {
         ArgumentNullException.ThrowIfNull(categories);
         ArgumentNullException.ThrowIfNull(recipes);
-
         for (int i = 0; i < recipes.Count; i++)
             for (int j = 0; j < recipes[i].Categories.Count; j++)
                 recipes[i].Categories[j] = categories.First(y => y.Id == recipes[i].Categories[j].Id);
 
         return recipes;
     }
+
     public static Recipe MatchCategory(Recipe recipe, List<Category> categories, List<Guid> categoriesIds)
     {
         ArgumentNullException.ThrowIfNull(recipe);
