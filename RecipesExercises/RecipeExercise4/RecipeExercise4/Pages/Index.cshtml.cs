85ea28d835c8abb186f4b959615b5c60b9e9b0bc
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-07-24 20:43:43 +0200

    inital commit

---
+++ RecipesExercises/RecipeExercise4/RecipeExercise4/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeExercise4.Models;
+
+
+namespace RecipeExercise4.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public string MyUrl;
+    public List<Recipe> Recipes { get; set; } = new();
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+        MyUrl = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("api")["url"];
+    }
+
+    public void OnGet()
+    {
+    }
+}
