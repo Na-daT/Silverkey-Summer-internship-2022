94249cb5538b10e5f4df409a1d258aa2eebc96bf
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-07-24 20:43:55 +0200

    css modifications

---
+++ RecipesExercises/RecipeExercise4/RecipeExercise4/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// // for details on configuring this project to bundle and minify static web assets.
+
+// // Write your JavaScript code.
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("navbar").style.top = "0";
+//     } else {
+//         document.getElementById("navbar").style.top = "-70px";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
