fa79d77e5655ff7bdbf573bb420a2c08cc4ad648
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-08-11 21:13:09 +0200

    created all recipe services

--- RecipesExercises/RecipeExercise5/Server/Services/RecipesService.cs
+++ RecipesExercises/RecipeExercise5/Server/Services/RecipesService.cs
@@ -25,4 +25,49 @@ public class RecipesService : Recipes.RecipesBase
             await responseStream.WriteAsync(response);
         }
     }
+
+    public override async Task<GetRecipeResponse> GetRecipe(GetRecipeRequest request, ServerCallContext context)
+    {
+        var recipe = _recipes.FirstOrDefault(r => r.Id == request.Id);
+        return new GetRecipeResponse { Recipe = recipe };
+    }
+
+    public override async Task<CreateRecipeResponse> CreateRecipe(CreateRecipeRequest request, ServerCallContext context)
+    {
+        var newRecipe = request.Recipe;
+        newRecipe.Id = Guid.NewGuid().ToString();
+        _recipes.Add(newRecipe);
+        Utility.SaveRecipes(_recipes);
+        return new CreateRecipeResponse { Id = newRecipe.Id };
+    }
+    
+    public override async Task<UpdateRecipeResponse> UpdateRecipe(UpdateRecipeRequest request, ServerCallContext context)
+    {
+        var recipeToUpdate = _recipes.FirstOrDefault(recipe => recipe.Id == request.Id);
+        if(recipeToUpdate is null)
+        {
+            return new UpdateRecipeResponse { Response = "fail" };
+        }
+        recipeToUpdate.Categories.Clear();
+        recipeToUpdate.Categories.AddRange(request.Recipe.Categories);
+        recipeToUpdate.Ingredients.Clear();
+        recipeToUpdate.Ingredients.AddRange(request.Recipe.Ingredients);
+        recipeToUpdate.Title = request.Recipe.Title;
+        recipeToUpdate.Instructions.Clear();
+        recipeToUpdate.Instructions.AddRange(request.Recipe.Instructions);
+        Utility.SaveRecipes(_recipes);
+        return new UpdateRecipeResponse { Response = "success" };
+    }
+
+    public override async Task<DeleteRecipeResponse> DeleteRecipe(DeleteRecipeRequest request, ServerCallContext context)
+    {
+        var recipeToDelete = _recipes.FirstOrDefault(r => r.Id == request.Id);
+        if(recipeToDelete is null)
+        {
+            return new DeleteRecipeResponse { Response = "fail" };
+        }
+        _recipes.Remove(recipeToDelete);
+        Utility.SaveRecipes(_recipes);
+        return new DeleteRecipeResponse { Response = "success" };
+    }
 }
