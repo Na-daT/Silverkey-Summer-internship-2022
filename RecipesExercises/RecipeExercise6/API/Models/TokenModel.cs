e6f18498e5f9973c557a78ddd075471b942f1a90
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-08-18 00:55:39 +0200

    clean up

--- RecipesExercises/RecipeExercise6/API/Models/TokenModel.cs
+++ RecipesExercises/RecipeExercise6/API/Models/TokenModel.cs
@@ -9,7 +9,6 @@ public interface ITokenService
 {
     string GenerateAccessToken(IEnumerable<Claim> claims);
     string GenerateRefreshToken();
-    //ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
 }
 
 public class TokenService : ITokenService
@@ -41,23 +40,4 @@ public class TokenService : ITokenService
             return Convert.ToBase64String(randomNumber);
         }
     }
-
-    //public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
-    //{
-    //    var tokenValidationParameters = new TokenValidationParameters
-    //    {
-    //        ValidateAudience = false, //you might want to validate the audience and issuer depending on your use case
-    //        ValidateIssuer = false,
-    //        ValidateIssuerSigningKey = true,
-    //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(myKey)),
-    //        ValidateLifetime = false //here we are saying that we don't care about the token's expiration date
-    //    };
-    //    var tokenHandler = new JwtSecurityTokenHandler();
-    //    SecurityToken securityToken;
-    //    var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
-    //    var jwtSecurityToken = securityToken as JwtSecurityToken;
-    //    if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
-    //        throw new SecurityTokenException("Invalid token");
-    //    return principal;
-    //}
 }
