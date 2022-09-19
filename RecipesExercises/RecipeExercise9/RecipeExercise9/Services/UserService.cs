32fd6215139eb1ac56c4ae9c29800645884f660d
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-09-19 22:56:33 +0200

    Create UserService.cs

---
+++ RecipesExercises/RecipeExercise9/RecipeExercise9/Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http.Json;
+namespace RecipeExercise9;
+public interface IUserService
+{
+    Task<HttpResponseMessage> Login(LoginModel loginDetails);
+    Task<HttpResponseMessage> Register(RegisterModel userDetails);
+    Task<HttpResponseMessage> RefreshToken(RefreshRequest request);
+}
+
+public class UserService : IUserService
+{
+    private readonly HttpClient _httpClient;
+    public UserService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<HttpResponseMessage> Login(LoginModel loginDetails)
+    {
+        try
+        {
+            return await _httpClient.PostAsJsonAsync("login", loginDetails);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    public async Task<HttpResponseMessage> Register(RegisterModel userDetails)
+    {
+        try
+        {
+            return await _httpClient.PostAsJsonAsync("register", userDetails);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    public async Task<HttpResponseMessage> RefreshToken(RefreshRequest request)
+    {
+        try
+        {
+            return await _httpClient.PostAsJsonAsync("refresh-token", request);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
