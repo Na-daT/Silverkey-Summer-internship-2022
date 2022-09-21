10af32b3004d5ccf613bc1b3abfd1c3b60db3db7
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-09-21 03:14:02 +0200

    URI error

--- RecipesExercises/RecipeExercise9/RecipeExercise9/Program.cs
+++ RecipesExercises/RecipeExercise9/RecipeExercise9/Program.cs
@@ -7,9 +7,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7047/api/json/") });
 
-builder.Services.AddSingleton<IRecipeService,RecipeService>();
+builder.Services.AddSingleton<IRecipeService, RecipeService>();
 builder.Services.AddSingleton<ICategoryService, CategoryService>();
 builder.Services.AddSingleton<IUserService, UserService>();
 
