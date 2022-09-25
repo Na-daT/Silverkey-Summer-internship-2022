8304e87cbe9867aa315d5a64c90db9f4bbcbd940
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-09-26 01:04:08 +0200

    RecipeCategory

--- RecipesExercises/RecipeExercise9/RecipeExercise9/Models/Recipe.cs
+++ RecipesExercises/RecipeExercise9/RecipeExercise9/Models/Recipe.cs
@@ -4,7 +4,7 @@ public class Recipe
     public string Title { get; set; }
     public List<Ingredient> Ingredients { get; set; } = new ();
     public List<Instruction> Instructions { get; set; } = new ();
-    public List<Category> Categories { get; set; } = new ();
+    public List<RecipeCategory> RecipeCategories { get; set; } = new ();
     public bool IsActive { get; set; }
 
     public Recipe()
@@ -12,13 +12,13 @@ public class Recipe
         Title = string.Empty;
     }
 
-    public Recipe(int id, string title, List<Ingredient> ingredients, List<Instruction> instructions, List<Category> categories, bool isActive)
+    public Recipe(int id, string title, List<Ingredient> ingredients, List<Instruction> instructions, List<RecipeCategory> categories, bool isActive)
     {
         Id = id;
         Title = title;
         Ingredients = ingredients;
         Instructions = instructions;
-        Categories = categories;
+        RecipeCategories = categories;
         IsActive = isActive;
     }
 }
