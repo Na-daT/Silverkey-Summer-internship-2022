bd2574b613c777a77f283df553decb2e113fda6f
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2022-09-26 01:04:01 +0200

    Create RecipeCategory.cs

---
+++ RecipesExercises/RecipeExercise9/RecipeExercise9/Models/RecipeCategory.cs
@@ -0,0 +1,17 @@
+﻿public class RecipeCategory
+{
+    public Category Category { get; set; } = new();
+    public int CategoryId { get; set; }
+    public bool IsActive { get; set; }
+    
+    public RecipeCategory()
+    {
+    }
+
+    public RecipeCategory(Category category, int categoryId, bool isActive)
+    {
+        Category = category;
+        CategoryId = categoryId;
+        IsActive = isActive;
+    }
+}
